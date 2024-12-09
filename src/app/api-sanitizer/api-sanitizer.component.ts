8af276c8deb6d3f175901c5eda478fee6193f386
Daniil Kiselev <dvkiselev@fil-it.ru>
2024-12-09 23:57:18 +0300

    finish with sanitizer

--- src/app/api-sanitizer/api-sanitizer.component.ts
+++ src/app/api-sanitizer/api-sanitizer.component.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { catchError, of } from 'rxjs';
+
+interface IResponce {
+  content: string
+}
 
 @Component({
   selector: 'api-sanitizer',
@@ -13,12 +18,17 @@ export class ApiSanitizerComponent implements OnInit {
   public sanitizedContent!: SafeHtml
 
   constructor(
-    // private _sanitized: DomSanitizer,
+    private _sanitized: DomSanitizer,
     private _http: HttpClient
   ) { }
 
   ngOnInit(): void {
-    this._http.get('assets/content.json').subscribe((resp) => console.log(resp))
+    this._http.get<IResponce>('assets/content.json').pipe(
+      catchError((err) => {
+        console.error('Ошибка при загрузке:', err)
+        return of({ content: '<p>Загрузка данных не удалась</p>' })
+      })
+    ).subscribe(({ content }) => this.sanitizedContent = this._sanitized.bypassSecurityTrustHtml(content))
   }
 
 
